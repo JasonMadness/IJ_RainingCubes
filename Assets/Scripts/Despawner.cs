8049e3d0707a8bf9d5b57a7369aac7da6b723761
JasonMadness <tobc_madness@mail.ru>
2026-09-22 20:58:32 +0300

    Update Despawner.cs

--- Assets/Scripts/Despawner.cs
+++ Assets/Scripts/Despawner.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 public class Despawner : MonoBehaviour
 {
     [SerializeField] private CubePool _cubePool;
+    [SerializeField] private BombSpawner _bombSpawner;
 
     private float _minDelay = 2.0f;
     private float _maxDelay = 5.0f;
 
     public void OnSurfaceTouched(Cube cube)
     {
-        StartCoroutine(ReturnToPoolAfterDelay(cube));
         cube.SurfaceTouched -= OnSurfaceTouched;
+        StartCoroutine(ReturnAndSpawnBomb(cube));
     }
 
-    private IEnumerator ReturnToPoolAfterDelay(Cube cube)
+    private IEnumerator ReturnAndSpawnBomb(Cube cube)
     {
-        float delay = Random.Range(_minDelay, _maxDelay);
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSeconds(Random.Range(_minDelay, _maxDelay));
+
+        Vector3 position = cube.transform.position;
         _cubePool.Return(cube);
+
+        _bombSpawner.SpawnAt(position);
     }
 }
